99129279830594227f8ed8512e08ba4f936d6197
Monemou Zator <monemouzator@gmail.com>
2026-09-06 17:35:50 +0000

    Mise à jour du projet

--- ecole/settings.py
+++ ecole/settings.py
@@ -158,12 +158,12 @@ if DEBUG:
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Nom et chemin de la base SQLite
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Nom et chemin de la base SQLite
+    }
+}
 
 
 
