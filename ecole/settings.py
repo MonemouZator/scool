b1f2d2845f404a614cad8b181ccb12a6ad5ee2bd
Monemou Zator <monemouzator@gmail.com>
2026-09-07 11:09:01 +0000

    Ajout messages vocaux et suppression messagerie

--- ecole/settings.py
+++ ecole/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = config('SECRET_KEY') #'django-insecure-52yfq))jd9r0uo*uq5p!j!4zccvrd13&hbd@3zq70$o^)+==%j'
 
-DEBUG =False #config('DEBUG', default=False, cast=bool)  # Mettre à False en production
+DEBUG =True #config('DEBUG', default=False, cast=bool)  # Mettre à False en production
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ecole-bnb.onrender.com']
 
