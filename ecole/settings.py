89d3159000fa596127bba62094d2b2ab8b9f63e7
Monemou Zator <monemouzator@gmail.com>
2026-09-10 10:47:05 +0000

    Configuration email pour Render

--- ecole/settings.py
+++ ecole/settings.py
@@ -182,12 +182,12 @@ if DEBUG:
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Nom et chemin de la base SQLite
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Nom et chemin de la base SQLite
+#     }
+# }
 
 
 
